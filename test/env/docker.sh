e3c228bc3fc43ac46ba65930eab9a6cb139c9a53
David J. Gardner <gardner48@llnl.gov>
2026-05-13 16:34:12 -0700

    update CI to test SLUDIST with single precision

--- test/env/docker.sh
+++ test/env/docker.sh
@@ -269,7 +269,13 @@ fi
 # SuperLU_DIST
 # ------------
 
-if [ "$SUNDIALS_PRECISION" == "double" ]; then
+if [ "$SUNDIALS_PRECISION" == "extended" ]; then
+    export SUNDIALS_SUPERLU_DIST=OFF
+    unset SUPERLU_DIST_INCLUDE_DIR
+    unset SUPERLU_DIST_LIBRARY_DIR
+    unset SUPERLU_DIST_LIBRARIES
+    unset SUPERLU_DIST_OPENMP
+else
     export SUNDIALS_SUPERLU_DIST=ON
     export SUPERLU_DIST_ROOT=/opt/view/superlu-dist
     export SUPERLU_DIST_INCLUDE_DIR="${SUPERLU_DIST_ROOT}/include"
@@ -277,24 +283,24 @@ if [ "$SUNDIALS_PRECISION" == "double" ]; then
 
     # build with netlib blas/lapack if using 64-bit indices so that we can
     # use 64-bit openblas for other TPLs
-    export BLAS_ROOT=/opt/view
+    SUPERLU_DIST_BLAS_ROOT=/opt/view
     if [ "$SUNDIALS_INDEX_SIZE" == "64" ]; then
-        if [ -f "${BLAS_ROOT}/lib/libblas.so" ]; then
-            export BLAS_LIBRARIES="${BLAS_ROOT}/lib/libblas.so;${BLAS_ROOT}/lib/liblapack.so"
+        if [ -f "${SUPERLU_DIST_BLAS_ROOT}/lib/libblas.so" ]; then
+            SUPERLU_DIST_BLAS_LIBRARIES="${SUPERLU_DIST_BLAS_ROOT}/lib/libblas.so;${SUPERLU_BLAS_BLAS_ROOT}/lib/liblapack.so"
         fi
     else
-        export BLAS_LIBRARIES="${BLAS_ROOT}/lib/libopenblas.so"
+        SUPERLU_DIST_BLAS_LIBRARIES="${SUPERLU_DIST_BLAS_ROOT}/lib/libopenblas.so"
     fi
 
     # PARMETIS
-    export PARMETIS_ROOT=/opt/view
-    export PARMETIS_LIBRARIES="${PARMETIS_ROOT}/lib/libparmetis.so"
+    PARMETIS_ROOT=/opt/view
+    PARMETIS_LIBRARIES="${PARMETIS_ROOT}/lib/libparmetis.so"
 
     # METIS
-    export METIS_ROOT=/opt/view
-    export METIS_LIBRARIES="${METIS_ROOT}/lib/libmetis.so"
+    METIS_ROOT=/opt/view
+    METIS_LIBRARIES="${METIS_ROOT}/lib/libmetis.so"
 
-    export SUPERLU_DIST_LIBRARIES="${BLAS_LIBRARIES};${PARMETIS_LIBRARIES};${METIS_LIBRARIES};${SUPERLU_DIST_ROOT}/lib/libsuperlu_dist.a"
+    export SUPERLU_DIST_LIBRARIES="${SUPERLU_DIST_BLAS_LIBRARIES};${PARMETIS_LIBRARIES};${METIS_LIBRARIES};${SUPERLU_DIST_ROOT}/lib/libsuperlu_dist.a"
     export SUPERLU_DIST_OPENMP=OFF
 
     # if BLAS wasn't found, then dont build SuperLU_DIST
@@ -303,12 +309,6 @@ if [ "$SUNDIALS_PRECISION" == "double" ]; then
     else
         export SUNDIALS_SUPERLU_DIST=ON
     fi
-else
-    export SUNDIALS_SUPERLU_DIST=OFF
-    unset SUPERLU_DIST_INCLUDE_DIR
-    unset SUPERLU_DIST_LIBRARY_DIR
-    unset SUPERLU_DIST_LIBRARIES
-    unset SUPERLU_DIST_OPENMP
 fi
 
 # -----
